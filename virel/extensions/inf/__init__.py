70ecc1c3f08936ce931add5c54f2b3c7903cd3e7
accurs <damon@azron.net>
2026-05-31 17:03:18 +0000

    feat: Add new commands for bot credits and invite link, enhancing user interaction and information accessibility

--- virel/extensions/inf/__init__.py
+++ virel/extensions/inf/__init__.py
@@ -1,7 +1,8 @@
 import sys
-
 import discord
-from discord import Embed
+
+from discord import Embed, ButtonStyle
+from discord.ui import Button, View
 from discord.ext.commands import Cog, command
 
 from virel.core import Virel, Context
@@ -18,7 +19,7 @@ class Information(Cog):
         """
         self.bot = bot
 
-    @command()
+    @command(aliases=["info", "about", "bi"])
     async def botinfo(self, ctx: Context):
         """
         Shows information about the bot.
@@ -59,6 +60,28 @@ class Information(Cog):
             inline=True,
         )
         return await ctx.send(embed=embed)
+
+    @command()
+    async def credits(self, ctx: Context):
+        """
+        Shows the credits for the bot.
+        """
+        embed = Embed(title="Credits", color=Configuration.Colors.neutral)
+        embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.display_avatar.url)
+        embed.description = (
+            f"[vael](https://discord.com/users/604463848526708757) - Developer"
+        )
+        return await ctx.send(embed=embed)
+
+    @command(aliases=["inv"])
+    async def invite(self, ctx: Context):
+        """
+        Shows the invite link for the bot.
+        """
+        view = View().add_item(
+            Button(label=f"Invite {self.bot.user.name}", url=f"https://discord.com/oauth2/authorize?client_id={self.bot.user.id}&permissions=8&scope=bot", style=ButtonStyle.link)
+        )
+        return await ctx.send(view=view)
 
 
 async def setup(bot: Virel):
